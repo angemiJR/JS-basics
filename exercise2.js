7465f25c2212b668922ed7b3ff0c98f7fbae2e91
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-10-23 15:26:45 +0200

    exercise 2 done

--- exercise2.js
+++ exercise2.js
@@ -1,6 +1,8 @@
 let name = `Emilija`;
-console.log(`My name is${name}.`)
+console.log(`My name is${name}.`);
 
 let age = 34;
 console.log(`I am ${age} years old.`);
 
+let FavoriteColor = `green`;
+console.log(`My favorite color is ${FavoriteColor}.`);
